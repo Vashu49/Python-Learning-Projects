5f748a8679c9fe2cb70ce10f05f38054f052d7fc
Vashu49 <144485577+Vashu49@users.noreply.github.com>
2024-04-20 20:57:52 +0530

    Add files via upload
    Fizz Buzz - Created a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.

---
+++ FizzBuzz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def fizz_buzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Test the function with numbers from 1 to 100
+fizz_buzz(100)
+
+
+# In[ ]:
+
+
+
+
